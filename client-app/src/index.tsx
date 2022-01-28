297aa11b145d14326172af2aa1bfdd23007323bc
stephanos48 <stephanos.english@gmail.com>
2022-01-28 15:26:18 -0600

    Add MobX & Axios

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
   //<React.StrictMode>
-    <App />,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
   //</React.StrictMode>,
   document.getElementById('root')
 );
